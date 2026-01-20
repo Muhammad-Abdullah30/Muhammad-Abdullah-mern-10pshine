31e26b7788571384cc2552017ea276168e1b24f8
Muhammad-Abdullah30 <143170087+Muhammad-Abdullah30@users.noreply.github.com>
2026-01-20 17:16:00 +0500

    Add signup page UI and API integration

--- frontend/myabnotes/src/App.js
+++ frontend/myabnotes/src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './pages/header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/auth/login/Login';
+import Signup from './pages/auth/signup/Signup';
+import Dashboard from './pages/dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header> </Header>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/Register" element={<Signup />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </>
   );
 }
 
