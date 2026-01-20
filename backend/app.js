a02f91270ead5764ef05bae5b99d01560ee39363
Muhammad-Abdullah30 <143170087+Muhammad-Abdullah30@users.noreply.github.com>
2026-01-20 17:11:40 +0500

    Add signup API with user registration logic

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,15 @@
 const express =require('express');
+const signupRoute = require('./routes/signup');
+const bodyParser = require('body-parser');
+const cors=require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/user",signupRoute)
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-});
+})
